2047effdd8b9076af918b794e46196bb050a1f3a
Johannes Brodwall <johannes@brodwall.com>
2024-08-04 13:10:14 +0200

    server.js create GET /api/tasks endpoint

--- server/server.js
+++ server/server.js
@@ -3,4 +3,8 @@ import express from "express";
 const app = express();
 app.listen(3000);
 
+app.get("/api/tasks", (req, res) => {
+    res.json([{ title: "create server" }]);
+})
+
 console.log("started http://localhost:3000");
