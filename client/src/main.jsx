8117f9eee6d52d6431051fd8fb97c8ca8b497ef5
Johannes Brodwall <johannes@brodwall.com>
2024-08-04 12:53:21 +0200

    convert application to React

---
+++ client/src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>PG6301 React Task Manager</h1>)
