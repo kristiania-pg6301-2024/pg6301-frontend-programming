16cc535763a9f8c7f678c6b831cefb1babe666bb
Johannes Brodwall <johannes@brodwall.com>
2024-08-04 13:24:38 +0200

    server.js: POST /api/tasks

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 
 const app = express();
-app.listen(3000);
+app.use(express.json());
 
+const tasks = [{ title: "create server" }];
 app.get("/api/tasks", (req, res) => {
-    res.json([{ title: "create server" }]);
+    res.json(tasks);
+})
+app.post("/api/tasks", (req, res) => {
+    const {title} = req.body;
+    tasks.push({title});
+    res.sendStatus(201);
 })
 
+
+app.listen(3000);
 console.log("started http://localhost:3000");
