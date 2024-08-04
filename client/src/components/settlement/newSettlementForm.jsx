24aceec16e31214ef4cfb8a9f7e533c1eb88ad78
Johannes Brodwall <johannes@brodwall.com>
2024-08-04 23:16:08 +0200

    Extract BillCountInput

--- client/src/components/settlement/newSettlementForm.jsx
+++ client/src/components/settlement/newSettlementForm.jsx
@@ -1,5 +1,28 @@
 import React, {useMemo, useState} from 'react';
 
+const DenominationTypes = {
+    "kr500": {
+        description: "femhundrelapper",
+    }
+}
+
+function BillCountInput({settlement, setSettlement, denomination}) {
+    return <div>
+        <div>Antall {DenominationTypes[denomination].description}:</div>
+        <input
+            type="number"
+            value={settlement.balance[denomination]?.count || ""}
+            onChange={(e) => setSettlement(old => ({
+                ...old,
+                balance: {
+                    ...old.balance,
+                    [denomination]: { count: e.target.value }
+                }
+            }))}
+        />
+    </div>;
+}
+
 export function NewSettlementForm() {
     const allDepartments = [
         "Auksjon", "Bilder", "Bøker", "Cafeteria", "Elektro", "Godteri", "Kjøkken", "Møbler", "Popcorn", "Sport",
@@ -21,7 +44,10 @@ export function NewSettlementForm() {
     const [settlement, setSettlement] = useState({
         balance: {}
     })
-    const sum = useMemo(() => settlement.balance["kr1000"]?.count * 1000 || 0, [settlement]);
+    const sum = useMemo(() => (
+        (settlement.balance["kr1000"]?.count || 0) * 1000
+        + (settlement.balance["kr500"]?.count || 0) * 500
+    ), [settlement]);
 
     return <form>
         <h2>Registrere kontanter</h2>
@@ -39,10 +65,11 @@ export function NewSettlementForm() {
             <div>Antall tusenlapper</div>
             <input type="text" value={settlement.balance["kr1000"]?.count || ""} onChange={e => handleChangeCount(e)}/>
         </div>
-        <div>
-            <div>Antall femhundrelapper:</div>
-            <input type="text"/>
-        </div>
+        <BillCountInput
+            settlement={settlement}
+            setSettlement={setSettlement}
+            denomination={"kr500"}
+        />
 
         <div>
             Sum: {sum}
