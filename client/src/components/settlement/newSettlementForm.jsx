3a6209c28516f9c54d749e03b930fbb3a8ce48f1
Johannes Brodwall <johannes@brodwall.com>
2024-08-04 23:37:37 +0200

    let user enter coins by weight instead of count

--- client/src/components/settlement/newSettlementForm.jsx
+++ client/src/components/settlement/newSettlementForm.jsx
@@ -27,18 +27,22 @@ const CoinDenominationTypes = {
     "kr20": {
         description: "20-kroninger",
         value: 20,
+        unitWeight: 9.9
     },
     "kr10": {
         description: "tikroninger",
-        value: 10
+        value: 10,
+        unitWeight: 6.8
     },
     "kr5": {
         description: "femkroninger",
-        value: 5
+        value: 5,
+        unitWeight: 7.85
     },
     "kr1": {
         description: "kronestykker",
-        value: 1
+        value: 1,
+        unitWeight: 4.35
     },
 };
 
@@ -60,19 +64,41 @@ function BillCountInput({settlement, setSettlement, denomination}) {
 }
 
 function CoinCountInput({settlement, setSettlement, denomination}) {
+    const count = settlement.balance[denomination]?.count;
+    const weight = settlement.balance[denomination]?.weight;
+    const coinType = CoinDenominationTypes[denomination];
     return <div>
-        <div>Antall {CoinDenominationTypes[denomination].description}:</div>
-        <input
-            type="number"
-            value={settlement.balance[denomination]?.count || ""}
-            onChange={(e) => setSettlement(old => ({
-                ...old,
-                balance: {
-                    ...old.balance,
-                    [denomination]: { count: e.target.value }
-                }
-            }))}
-        />
+        <div>{coinType.description}:</div>
+        <div>
+            Antall:
+            <input
+                type="number"
+                disabled={weight}
+                value={count || Math.round(weight/coinType.unitWeight) || ""}
+                onChange={(e) => setSettlement(old => ({
+                    ...old,
+                    balance: {
+                        ...old.balance,
+                        [denomination]: {count: e.target.value}
+                    }
+                }))}
+            />
+            {" "}
+            Vekt:
+            <input
+                type="number"
+                disabled={count}
+                value={weight || count*coinType.unitWeight || ""}
+                onChange={(e) => setSettlement(old => ({
+                    ...old,
+                    balance: {
+                        ...old.balance,
+                        [denomination]: {weight: e.target.value}
+                    }
+                }))}
+            />
+            g
+        </div>
     </div>;
 }
 
@@ -84,8 +110,11 @@ function sumBalance(balance) {
         }
     }
     for (const denonimation in CoinDenominationTypes) {
+        const type = CoinDenominationTypes[denonimation];
         if (balance[denonimation]?.count) {
-            sum += balance[denonimation].count * CoinDenominationTypes[denonimation].value;
+            sum += balance[denonimation].count * type.value;
+        } else if (balance[denonimation]?.weight) {
+            sum += Math.round(balance[denonimation].weight/type.unitWeight) * type.value;
         }
     }
     return sum;
@@ -99,7 +128,7 @@ export function NewSettlementForm() {
     const [settlement, setSettlement] = useState({
         balance: {}
     })
-    const sum = useMemo(() => sumBalance(settlement.balance), [settlement]);
+    const sum = sumBalance(settlement.balance);
 
     return <form>
         <h2>Registrere kontanter</h2>
@@ -130,7 +159,7 @@ export function NewSettlementForm() {
             />
         )}
         <div>
-            Sum: {sum}
+            <strong>Sum: {sum}</strong>
         </div>
 
         <pre>
