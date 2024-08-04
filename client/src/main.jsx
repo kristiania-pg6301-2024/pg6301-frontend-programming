2b198dcff0465632f47e47d12c52b91152d0267b
Johannes Brodwall <johannes@brodwall.com>
2024-08-04 12:55:35 +0200

    show AddTaskForm

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>PG6301 React Task Manager</h1>)
+
+function AddTaskForm() {
+    return <form>
+        <div>
+            <label>
+                Task:
+            </label>
+            <input/>
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>;
+}
+
+function TaskManagerApplication() {
+    return <>
+        <h1>PG6301 React Task Manager</h1>
+        <h2>Existing tasks</h2>
+        <h2>Create new tasks</h2>
+        <AddTaskForm/>
+    </>;
+}
+
+root.render(<TaskManagerApplication/>)
