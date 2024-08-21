0a803ea65eb5a83d8456477209d6f2ac34ab06d0
Johannes Brodwall <johannes@brodwall.com>
2024-08-21 13:36:15 +0200

    Implement /api/tasks

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/tasks", (req, res) => {
+    res.send([{
+        title: "the task from the server"
+    }])
+})
+
+
 app.listen(3000);
 
 
