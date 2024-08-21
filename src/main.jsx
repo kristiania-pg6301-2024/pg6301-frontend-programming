2c93c52d68d822d9d27be1567232a7965658b3a1
Johannes Brodwall <johannes@brodwall.com>
2024-08-21 12:51:48 +0200

    NewTaskForm

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,24 @@ import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<h1>Hello Frontend Students</h1>)
+function NewTaskForm() {
+    return <form>
+        <div>
+        <label>
+            Title: <input type="text"/>
+        </label>
+        </div>
+        <button>Submit</button>
+    </form>;
+}
+
+function TaskApplication() {
+    return <>
+        <h1>Here are the tasks we need to do!</h1>
+
+        <NewTaskForm />
+
+        </>;
+}
+
+root.render(<TaskApplication />);
