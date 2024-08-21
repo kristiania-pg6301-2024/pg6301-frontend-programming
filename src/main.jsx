769c535065d03e49ab4fe3fe017550e74baf2a3e
Johannes Brodwall <johannes@brodwall.com>
2024-08-21 12:48:52 +0200

    React application

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {createRoot} from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<h1>Hello Frontend Students</h1>)
