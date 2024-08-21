c1b61b9c2a29922add92a215da34c8a59f0737bc
Johannes Brodwall <johannes@brodwall.com>
2024-08-21 12:59:00 +0200

    NewTaskForm now sort of working, but not explained

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-function NewTaskForm() {
-    return <form>
+function NewTaskForm({onNewTask}) {
+
+    const [title, setTitle] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Submitting task", { title });
+        onNewTask({title})
+    }
+
+    return <form onSubmit={handleSubmit}>
         <div>
         <label>
-            Title: <input type="text"/>
+            Title: <input
+                type="text"
+                value={title}
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+        />
         </label>
         </div>
-        <button>Submit</button>
+        <button>Submit new task "{title}"</button>
     </form>;
 }
 
 function TaskApplication() {
+    const [tasks, setTasks] = useState([])
+
+    function handleNewTask(task) {
+        setTasks((prevTasks) => [...prevTasks, task]);
+    }
+
     return <>
         <h1>Here are the tasks we need to do!</h1>
 
-        <NewTaskForm />
+        <ul>
+            {tasks.map((task) => (<li>{task.title}</li>))}
+        </ul>
+
+        <NewTaskForm onNewTask={handleNewTask} />
 
         </>;
 }
