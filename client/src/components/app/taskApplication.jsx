8487bb2f80399df9eae5bb0bea21e71e6d518f5c
Johannes Brodwall <johannes@brodwall.com>
2024-08-27 23:03:02 +0200

    copy static example from exercise text

--- client/src/components/app/taskApplication.jsx
+++ client/src/components/app/taskApplication.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export function TaskApplication() {
-    return <h1>React task application</h1>;
+    const tasks = [
+        { id: 1, description: "Follow the lecture", completed: true },
+        { id: 2, description: "Read the exercise", completed: false },
+        { id: 3, description: "Complete the exercise", completed: true },
+    ];
+
+    return <div>
+        <h2>Tasks</h2>
+        {tasks.map(({id, description, completed}) => <label key={id}>
+            <input type="checkbox" checked={completed} />
+            {description}
+        </label>)}
+    </div>;
 }
