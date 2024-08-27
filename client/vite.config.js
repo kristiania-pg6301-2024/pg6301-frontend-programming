5dc7b132374c9a75bcf5ab37b02339197e364690
Johannes Brodwall <johannes@brodwall.com>
2024-08-28 00:45:14 +0200

    proxy /api request through Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,10 @@ import {defineConfig} from "vite";
 import reactVite from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [reactVite()]
+    plugins: [reactVite()],
+    server: {
+        proxy: {
+            "/api": "http://localhost:3000"
+        }
+    }
 })
