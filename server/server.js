28b81d9b6e96e6ab1553f1584e57a5851cc7e442
Johannes Brodwall <johannes@brodwall.com>
2024-08-28 01:16:49 +0200

    implement default route (buggy!)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import {tasksApi} from "./tasksApi.js";
+import * as path from "node:path";
 
 const app = express();
 app.use(express.json());
 app.use(tasksApi);
 app.use(express.static("../client/dist"));
+
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/dist/index.html"));
+    }
+})
+
 
 app.listen(3000);
