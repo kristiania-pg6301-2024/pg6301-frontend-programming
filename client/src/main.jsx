b05435cd26108a7e34df3ced93932f3e2423f99f
Johannes Brodwall <johannes@brodwall.com>
2024-09-03 15:47:30 +0200

    Static HTML prototype

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,37 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello Class</h1>);
+root.render(<Application/>);
+
+function Application() {
+    return <>
+        <h1>Register cash settlement</h1>
+
+        <div>
+            Department:
+            <select>
+                <option value="">Cafe</option>
+                <option value="">Furniture</option>
+                <option value="">Books</option>
+                <option value="">Clothes</option>
+            </select>
+        </div>
+        <div>
+            1000-lapper:{" "}
+            <input type="number"/>
+        </div>
+        <div>
+            500-lapper:{" "}
+            <input type="number"/>
+        </div>
+        <div>
+            200-lapper:{" "}
+            <input type="number"/>
+        </div>
+        <div>
+            Sum: 2800 kr
+        </div>
+    </>
+}
 
 
