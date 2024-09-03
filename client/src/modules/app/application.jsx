2a678505fe96a4b883df4082ffa76560903d29f6
Johannes Brodwall <johannes@brodwall.com>
2024-09-03 15:59:04 +0200

    getting ready to submit settlement report

--- client/src/modules/app/application.jsx
+++ client/src/modules/app/application.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 export function Application() {
-    const departments = [
+    const [selectedDepartment, setSelectedDepartment] = useState("Furniture");
+
+    const [kr1000, setKr1000] = useState("")
+
+    const departmentOptions = [
         "Cafe",
         "Furniture",
         "Books",
         "Clothes",
     ];
 
+    const settlementReport = {
+        selectedDepartment,
+        bills: {
+            "1000kr": kr1000
+        }
+    };
     return <form>
         <h1>Register cash settlement</h1>
 
         <div>
             Department:
-            <select>
-                {departments.map(d => <option>{d}</option>)}
+            <select value={selectedDepartment} onChange={e => setSelectedDepartment(e.target.value)}>
+                {departmentOptions.map(d => <option>{d}</option>)}
             </select>
         </div>
         <div>
             1000-lapper:{" "}
-            <input type="number"/>
+            <input type="number" value={kr1000} onChange={e => setKr1000(e.target.value)}/>
         </div>
         <div>
             500-lapper:{" "}
@@ -34,6 +44,9 @@ export function Application() {
         </div>
         <div>
             <button>Submit</button>
+        </div>
+        <div>
+            <pre>{JSON.stringify(settlementReport)}</pre>
         </div>
     </form>
 }
