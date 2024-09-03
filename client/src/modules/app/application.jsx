7f38df88c233489e6ce6b13c4fc28a5ada9f6633
Johannes Brodwall <johannes@brodwall.com>
2024-09-03 16:34:50 +0200

    it's working, but kind of messy

--- client/src/modules/app/application.jsx
+++ client/src/modules/app/application.jsx
@@ -1,9 +1,48 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
+
+function BillInput(props) {
+    const denomination = props.denomination;
+    const label = props.label;
+    const balance = props.balance;
+    const setBalance = props.setBalance;
+
+    return <div>
+        {label}:{" "}
+        <input
+            type="number"
+            value={balance[denomination]}
+            onChange={e => setBalance(old => ({
+                ...old,
+                [denomination]: e.target.value
+            }))}
+        />
+    </div>;
+}
 
 export function Application() {
     const [selectedDepartment, setSelectedDepartment] = useState("Furniture");
 
-    const [kr1000, setKr1000] = useState("")
+    const [balance, setBalance] = useState({
+        ["1000kr"]: "0",
+        ["500kr"]: "0",
+        ["200kr"]: "0",
+    });
+
+    const values = {
+        ["1000kr"]: 1000,
+        ["500kr"]: 500,
+        ["200kr"]: 200,
+        ["100kr"]: 100,
+        ["50kr"]: 50,
+    }
+
+    const sum = useMemo(() => {
+        let result = 0;
+        for (const denomination in balance) {
+            result += values[denomination] * balance[denomination];
+        }
+        return result;
+    }, [balance])
 
     const departmentOptions = [
         "Cafe",
@@ -14,9 +53,7 @@ export function Application() {
 
     const settlementReport = {
         selectedDepartment,
-        bills: {
-            "1000kr": kr1000
-        }
+        balance
     };
     return <form>
         <h1>Register cash settlement</h1>
@@ -28,19 +65,47 @@ export function Application() {
             </select>
         </div>
         <div>
-            1000-lapper:{" "}
-            <input type="number" value={kr1000} onChange={e => setKr1000(e.target.value)}/>
+            Antall 1000-lapper:{" "}
+            <input
+                type="number"
+                value={balance["1000kr"]}
+                onChange={e => setBalance(old => ({
+                    ...old,
+                    ["1000kr"]: e.target.value
+                }))}
+            />
         </div>
         <div>
-            500-lapper:{" "}
-            <input type="number"/>
+            Antall 500-lapper:{" "}
+            <input
+                type="number"
+                value={balance["500kr"]}
+                onChange={e => setBalance(old => ({
+                    ...old,
+                    ["500kr"]: e.target.value
+                }))}
+            />
         </div>
+        <BillInput
+            denomination={"200kr"}
+            label={"200-lapper"}
+            balance={balance}
+            setBalance={setBalance}
+        />
+        <BillInput
+            denomination={"100kr"}
+            label={"100-lapper"}
+            balance={balance}
+            setBalance={setBalance}
+        />
+        <BillInput
+            denomination={"50kr"}
+            label={"50-lapper"}
+            balance={balance}
+            setBalance={setBalance}
+        />
         <div>
-            200-lapper:{" "}
-            <input type="number"/>
-        </div>
-        <div>
-            Sum: 2800 kr
+            Sum: {sum} kr
         </div>
         <div>
             <button>Submit</button>
