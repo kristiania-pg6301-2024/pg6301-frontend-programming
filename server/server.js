6d99f52b16843d6e0f00769b0ec851d4b782900a
Johannes Brodwall <johannes@brodwall.com>
2024-09-11 00:05:29 +0200

    extract settlementApi

--- server/server.js
+++ server/server.js
@@ -4,16 +4,18 @@ const app = express();
 app.use(express.json());
 
 const settlements = [
-    {id: 0, selectedDepartment: "Furniture", balance: {"20kr": 200, "10kr": 50}}
+    {id: 0, selectedDepartment: "Furniture", balance: {"20kr": 200, "10kr": 51}}
 ];
-app.get("/api/settlements", (req, res) => {
+const settlementApi = express.Router();
+settlementApi.get("/", (req, res) => {
     res.json(settlements)
 })
-app.post("/api/settlements", (req, res) => {
+settlementApi.post("/", (req, res) => {
     const { selectedDepartment, balance } = req.body;
     const id = settlements.length;
     settlements.push({id, selectedDepartment, balance});
     res.sendStatus(201);
 })
+app.use("/api/settlements", settlementApi);
 
 app.listen(3000);
