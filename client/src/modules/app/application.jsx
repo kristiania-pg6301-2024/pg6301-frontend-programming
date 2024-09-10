cf16290dbd36aebff92ae0310d6b9985ed4132f2
Johannes Brodwall <johannes@brodwall.com>
2024-09-10 23:39:48 +0200

    extract functions handleNewSettlement and loadSettlements

--- client/src/modules/app/application.jsx
+++ client/src/modules/app/application.jsx
@@ -1,15 +1,28 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {SubmitSettlementForm} from "../settlement/submitSettlementForm";
 import {sumBalance} from "../settlement/money";
 import {Dialog} from "../components/dialog.jsx";
 
 
 export function Application() {
-    const [settlements, setSettlements] = useState([
-            {selectedDepartment: "Furniture", balance: {"10kr": 2000}}
-        ]
-    );
+    const [settlements, setSettlements] = useState([]);
+
+    function handleNewSettlement(s) {
+        setSettlements(old => [...old, s]);
+        setShowsSettlementDialog(false);
+    }
+    function loadSettlements() {
+        setSettlements([
+            { selectedDepartment: "Furniture", balance:  { "10kr": 200 } }
+        ])
+    }
+
+    useEffect(() => {
+        loadSettlements();
+    }, [])
+
     const [showsSettlementDialog, setShowsSettlementDialog] = useState(false)
+
     return <>
         <h1>Settlements</h1>
         {settlements.map(s => <div>
@@ -17,10 +30,7 @@ export function Application() {
         </div>)}
 
         <Dialog visible={showsSettlementDialog} onClose={() => setShowsSettlementDialog(false)}>
-            <SubmitSettlementForm onNewSettlement={s => {
-                setSettlements(old => [...old, s]);
-                setShowsSettlementDialog(false);
-            }}/>
+            <SubmitSettlementForm onNewSettlement={handleNewSettlement}/>
             <button onClick={() => setShowsSettlementDialog(false)}>Cancel</button>
         </Dialog>
         <button onClick={() => setShowsSettlementDialog(true)}>Create new settlement</button>
