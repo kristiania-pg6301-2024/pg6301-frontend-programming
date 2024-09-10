5405234275e407586ac94141f37de2a31946077c
Johannes Brodwall <johannes@brodwall.com>
2024-09-10 23:54:16 +0200

    Express part 2: return settlements from server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,18 @@
 import express from "express";
 
 const app = express();
+
+app.get("/api/settlements", (req, res) => {
+    res.json([
+        {
+            id: 0,
+            selectedDepartment: "Furniture",
+            balance: {
+                "20kr": 200,
+                "10kr": 50
+            }
+        }
+    ])
+})
+
 app.listen(3000);
