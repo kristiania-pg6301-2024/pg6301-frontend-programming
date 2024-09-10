aded4b2674c6c89c0da84e1bd305e5e4e4dcf873
Johannes Brodwall <johannes@brodwall.com>
2024-09-11 00:26:00 +0200

    server: send default file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import {settlementApi} from "./settlementApi.js";
+import * as path from "node:path";
 
 const app = express();
 app.use(express.json());
 app.use("/api/settlements", settlementApi);
 app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/dist/index.html"))
+    } else {
+        next();
+    }
+})
 
 app.listen(3000);
