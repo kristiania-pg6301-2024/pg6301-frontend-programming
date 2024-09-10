44744fcaa3fdb497e2c7df73c06dfe9e942cc501
Johannes Brodwall <johannes@brodwall.com>
2024-09-11 00:09:37 +0200

    serve client assets in server.js

--- server/server.js
+++ server/server.js
@@ -4,5 +4,6 @@ import {settlementApi} from "./settlementApi.js";
 const app = express();
 app.use(express.json());
 app.use("/api/settlements", settlementApi);
+app.use(express.static("../client/dist"));
 
 app.listen(3000);
