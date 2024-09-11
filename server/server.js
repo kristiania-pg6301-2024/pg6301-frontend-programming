dc97bbc6164044af2ece7d4d3cb63a9a3d21ca3b
Johannes Brodwall <johannes@brodwall.com>
2024-09-11 13:39:34 +0200

    introduce express.static to serve React from Express

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import {settlementApi} from "./settlementApi.js";
 const app = express();
 app.use(express.json());
 app.use("/api/settlements", settlementApi);
+app.use(express.static("../client/dist"));
 
 app.listen(3000);
 
