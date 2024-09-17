0410def39a048cc4885fd5763288b46c17d9267f
Johannes Brodwall <johannes@brodwall.com>
2024-09-18 00:21:47 +0200

    `npm install -D husky prettier && npx husky init && npx prettier --write . && npm pkg set scripts.test='prettier --check . && vitest --run'`

--- shared/money.test.ts
+++ shared/money.test.ts
@@ -1,9 +1,9 @@
-import {expect, test} from "vitest";
+import { expect, test } from "vitest";
 
 function sum(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
 test("calculate sum", () => {
-    expect(sum(1, 2)).toBe(3);
-})
+  expect(sum(1, 2)).toBe(3);
+});
