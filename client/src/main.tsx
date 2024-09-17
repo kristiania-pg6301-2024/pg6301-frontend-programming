e46cf63f4075a99410e571fdbeb6669541d8542f
Johannes Brodwall <johannes@brodwall.com>
2024-09-17 23:16:01 +0200

    add types to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Application } from "./modules/app/application";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <BrowserRouter>
     <Application />
