03e5ab64f2a078cdb96c2e0c797c7afd0f7ae05b
Johannes Brodwall <johannes@brodwall.com>
2024-09-18 00:44:33 +0200

    add typescript

--- shared/money.ts
+++ shared/money.ts
@@ -1,14 +1,21 @@
 type BillDenomination = "1000kr" | "500kr" | "100kr" | "50kr";
-const BillTypes = [
+const BillTypes: {
+  key: BillDenomination;
+  value: number;
+}[] = [
   { key: "1000kr", value: 1000 },
   { key: "500kr", value: 500 },
   { key: "100kr", value: 100 },
   { key: "50kr", value: 50 },
 ];
 type CoinDenomination = "20kr" | "10kr";
-const CoinTypes = [
+const CoinTypes: {
+  key: CoinDenomination;
+  value: number;
+  gramsPerCoin: number;
+}[] = [
   { key: "20kr", value: 20, gramsPerCoin: 9.9 },
-  { key: "10kr", value: 10 },
+  { key: "10kr", value: 10, gramsPerCoin: 6.8 },
 ];
 type Coins = { grams: number } | { count: number };
 type Balance = Partial<Record<BillDenomination, number>> &
@@ -17,18 +24,14 @@ type Balance = Partial<Record<BillDenomination, number>> &
 export function sumBalance(balance: Balance): number {
   let sum = 0;
   for (const billType of BillTypes) {
-    if (billType.key in balance) {
-      sum += balance[billType.key] * billType.value;
-    }
+    sum += (balance[billType.key] || 0) * billType.value;
   }
   for (const coinType of CoinTypes) {
-    if (coinType.key in balance) {
-      const amount: Coins = balance[coinType.key];
-      if ("grams" in amount) {
-        sum += (amount.grams / coinType.gramsPerCoin) * coinType.value;
-      } else {
-        sum += amount.count * coinType.value;
-      }
+    const coins: Coins = balance[coinType.key] || { count: 0 };
+    if ("grams" in coins) {
+      sum += (coins.grams / coinType.gramsPerCoin) * coinType.value;
+    } else {
+      sum += coins.count * coinType.value;
     }
   }
   return sum;
