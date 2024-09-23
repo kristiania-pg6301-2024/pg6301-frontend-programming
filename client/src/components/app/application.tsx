06924ef4cdf8d6f23a5eb772ad23331d290e01d7
Johannes Brodwall <johannes@brodwall.com>
2024-09-24 00:45:24 +0200

    refactoring to demonstrate async

--- client/src/components/app/application.tsx
+++ client/src/components/app/application.tsx
@@ -6,22 +6,28 @@ interface Settlement {
   balance: Record<string, number>;
 }
 
+const sampleSettlements: Settlement[] = [
+  {
+    id: 1,
+    department: "furniture",
+    balance: { "1000kr": 10, "200kr": 15 },
+  },
+  {
+    id: 2,
+    department: "cafeteria",
+    balance: { "200kr": 100, "100kr": 15 },
+  },
+];
+
 export function Application() {
   const [settlements, setSettlements] = useState<Settlement[]>([]);
 
+  function loadSettlements() {
+    setSettlements(sampleSettlements);
+  }
+
   useEffect(() => {
-    setSettlements([
-      {
-        id: 1,
-        department: "furniture",
-        balance: { "1000kr": 10, "200kr": 15 },
-      },
-      {
-        id: 2,
-        department: "cafeteria",
-        balance: { "200kr": 100, "100kr": 15 },
-      },
-    ]);
+    loadSettlements();
   }, []);
 
   return (
