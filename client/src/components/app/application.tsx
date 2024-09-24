974be3cece77e4c5e9eab14974b8177f0dd160f2
Johannes Brodwall <johannes@brodwall.com>
2024-09-24 16:25:00 +0200

    fetchSettlements with promises

--- client/src/components/app/application.tsx
+++ client/src/components/app/application.tsx
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 interface Settlement {
+  id: number;
   department: string;
   balance: Record<string, number>;
+}
+
+const sampleSettlements: Settlement[] = [
+  { id: 0, department: "furniture", balance: { "1000kr": 3, "200kr": 80 } },
+  { id: 1, department: "cafeteria", balance: { "100kr": 50, "50kr": 60 } },
+];
+
+function timeout(millis: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("hei");
+    }, millis);
+  });
+}
+
+function fetchSettlements(): Promise<Settlement[]> {
+  return timeout(1000).then(() => sampleSettlements);
 }
 
 export function Application() {
   const [settlements, setSettlements] = useState<Settlement[]>([]);
 
-  function loadSettlements() {
-    setTimeout(() => {
-      setSettlements([
-        { department: "furniture", balance: { "1000kr": 3, "200kr": 80 } },
-        { department: "cafeteria", balance: { "100kr": 50, "50kr": 60 } },
-      ]);
-    }, 1000);
+  async function loadSettlements() {
+    setSettlements([]);
+    fetchSettlements().then((settlements) => {
+      setSettlements(settlements);
+    });
   }
 
   useEffect(() => {
@@ -25,8 +41,11 @@ export function Application() {
     <>
       <h1>Dugnadsoppgjør</h1>
       {settlements.map((s) => (
-        <div>{s.department}</div>
+        <div key={s.id}>{s.department}</div>
       ))}
+      <div>
+        <button onClick={() => loadSettlements()}>Refresh</button>
+      </div>
     </>
   );
 }
