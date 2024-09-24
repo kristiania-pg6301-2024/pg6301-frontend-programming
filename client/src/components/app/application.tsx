a7e2f1225a5f54c3b6b252a1b63455bf76edfbcb
Johannes Brodwall <johannes@brodwall.com>
2024-09-24 16:00:08 +0200

    simulate slow server

--- client/src/components/app/application.tsx
+++ client/src/components/app/application.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+interface Settlement {
+  department: string;
+  balance: Record<string, number>;
+}
 
 export function Application() {
-  const settlements = [
-    { departments: "furniture", balance: { "1000kr": 3, "200kr": 80 } },
-    { departments: "cafeteria", balance: { "100kr": 50, "50kr": 60 } },
-  ];
+  const [settlements, setSettlements] = useState<Settlement[]>([]);
+
+  function loadSettlements() {
+    setTimeout(() => {
+      setSettlements([
+        { department: "furniture", balance: { "1000kr": 3, "200kr": 80 } },
+        { department: "cafeteria", balance: { "100kr": 50, "50kr": 60 } },
+      ]);
+    }, 1000);
+  }
+
+  useEffect(() => {
+    loadSettlements();
+  }, []);
 
   return (
     <>
       <h1>Dugnadsoppgjør</h1>
       {settlements.map((s) => (
-        <div>{s.departments}</div>
+        <div>{s.department}</div>
       ))}
     </>
   );
