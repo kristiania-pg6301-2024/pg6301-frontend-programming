51a596f9ef7aac7b4531d543f3643296aca5a99b
Johannes Brodwall <johannes@brodwall.com>
2024-09-25 13:29:58 +0200

    add some experimental code for mongodb

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,18 @@
 import express from "express";
 import { settlementApi } from "./settlementApi";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(
+  "mongodb+srv://jhannes:TmOvOj4SBU61OBgm@cluster0.zwzty.mongodb.net/",
+);
+client.connect().then(async (connection) => {
+  const db = connection.db("dugnad");
+  const result = await db
+    .collection("settlements")
+    .find({ department: "books" })
+    .toArray();
+  console.log(result);
+});
 
 const app = express();
 app.use(express.json());
