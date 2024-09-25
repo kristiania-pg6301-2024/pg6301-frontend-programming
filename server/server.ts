ca994fa7f9ddc39861660048fd12344efadaae8a
Johannes Brodwall <johannes@brodwall.com>
2024-09-25 13:50:50 +0200

    move secret configuration to environment variables

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { settlementApi } from "./settlementApi";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
-const client = new MongoClient(
-  "mongodb+srv://jhannes:TmOvOj4SBU61OBgm@cluster0.zwzty.mongodb.net/",
-);
+const client = new MongoClient(process.env["MONGODB_URL"]!);
 client.connect().then(async (connection) => {
   const db = connection.db("dugnad");
   const result = await db
