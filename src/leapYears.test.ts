07cfe2efb86e0719cf95b12ae498fe8ce0815b99
Johannes Brodwall <johannes@brodwall.com>
2024-09-30 17:48:05 +0200

    🔴 add `src/leapYears.test.ts` (fails)

--- src/leapYears.test.ts
+++ src/leapYears.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, it } from "vitest";
 
+function isLeapYear(number: number) {
+
+}
+
 describe("leap years", () => {
   it("returns false for normal years", () => {
     expect(isLeapYear(2025)).toBe(false);
