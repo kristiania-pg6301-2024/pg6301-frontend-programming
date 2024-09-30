c3bc65b27d57e2f19fb3ddf8489e9242e5ee0052
Johannes Brodwall <johannes@brodwall.com>
2024-09-30 17:52:21 +0200

    🔴 leap years returns false for years divisible by 100

--- src/leapYears.test.ts
+++ src/leapYears.test.ts
@@ -11,4 +11,7 @@ describe("leap years", () => {
   it("returns true for years divisible by four", () => {
     expect(isLeapYear(2024)).toBe(true);
   });
+  it("returns false for years divisible by 100", () => {
+    expect(isLeapYear(2100)).toBe(false);
+  });
 });
