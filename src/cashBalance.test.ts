3406c64a4e3b8ae823cf76673a2c35e220aa86f3
Johannes Brodwall <johannes@brodwall.com>
2024-10-01 10:35:16 +0200

    🟢 sum balances
    * remove `vite.config.js` as we want to watch in IDE
    * change husky to use its own script
    * include prettier in pre-commit
    * prettify test.yaml

--- src/cashBalance.test.ts
+++ src/cashBalance.test.ts
@@ -1,11 +1,14 @@
 import { describe, expect, it } from "vitest";
 
-function sumBalance(param: { "1000kr": number }) {
-  return 1000 * param["1000kr"];
+function sumBalance(param: { "50kr"?: number; "1000kr"?: number }) {
+  return 1000 * (param["1000kr"] || 0) + 50 * (param["50kr"] || 0);
 }
 
 describe("cash balance", () => {
   it("calculates number of bills", () => {
     expect(sumBalance({ "1000kr": 11 })).toBe(11_000);
+  });
+  it("sums bills of different type", () => {
+    expect(sumBalance({ "1000kr": 1, "50kr": 8 })).toBe(1400);
   });
 });
