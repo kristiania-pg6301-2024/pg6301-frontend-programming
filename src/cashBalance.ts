26f60e0253dc08c646a8321c27b8aef40f99ac9d
Johannes Brodwall <johannes@brodwall.com>
2024-10-01 10:38:14 +0200

    🔵 convert interface CashBalance to type alias

--- src/cashBalance.ts
+++ src/cashBalance.ts
@@ -1,7 +1,4 @@
-interface CashBalance {
-  "50kr"?: number;
-  "1000kr"?: number;
-}
+type CashBalance = { "50kr"?: number; "1000kr"?: number };
 
 export function sumBalance(balance: CashBalance) {
   return 1000 * (balance["1000kr"] || 0) + 50 * (balance["50kr"] || 0);
