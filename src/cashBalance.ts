75759d176f9426da1c37d5da6e3a62da386d0776
Johannes Brodwall <johannes@brodwall.com>
2024-10-01 10:42:44 +0200

    ci: introduce typescript
    * `npm install -D typescript`
    * `npx tsc --init`

--- src/cashBalance.ts
+++ src/cashBalance.ts
@@ -1,6 +1,6 @@
 type BillDenominations = "50kr" | "1000kr";
 
-type CashBalance = Record<BillDenominations, number>;
+type CashBalance = Partial<Record<BillDenominations, number>>;
 
 export function sumBalance(balance: CashBalance) {
   return 1000 * (balance["1000kr"] || 0) + 50 * (balance["50kr"] || 0);
