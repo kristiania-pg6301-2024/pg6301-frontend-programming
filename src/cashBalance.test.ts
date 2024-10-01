2abe47e291b5d32407fb766e01f14e0623030ade
Johannes Brodwall <johannes@brodwall.com>
2024-10-01 10:12:20 +0200

    `npm i -D husky && npx husky init`
    * commit hangs because of failed test
    * commit hangs because of waiting test
    * vite.config.js skips watch

--- src/cashBalance.test.ts
+++ src/cashBalance.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, it } from "vitest";
 
-function sumBalance(param: { "1000kr": number }) {}
+function sumBalance(param: { "1000kr": number }) {
+  return 1000 * param["1000kr"];
+}
 
 describe("cash balance", () => {
   it("calculates number of bills", () => {
-    expect(sumBalance({ "1000kr": 11 })).toBe(10_000);
+    expect(sumBalance({ "1000kr": 11 })).toBe(11_000);
   });
 });
