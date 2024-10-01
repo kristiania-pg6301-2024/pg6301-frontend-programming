b6fd7ae7400945a38e482637b9523ad0435f9ba1
Johannes Brodwall <johannes@brodwall.com>
2024-10-01 10:39:38 +0200

    🔵 convert type alias CashBalance to Record

--- src/cashBalance.ts
+++ src/cashBalance.ts
@@ -1,4 +1,6 @@
-type CashBalance = { "50kr"?: number; "1000kr"?: number };
+type BillDenominations = "50kr" | "1000kr";
+
+type CashBalance = Record<BillDenominations, number>;
 
 export function sumBalance(balance: CashBalance) {
   return 1000 * (balance["1000kr"] || 0) + 50 * (balance["50kr"] || 0);
