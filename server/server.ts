369a91da0c224da4d7e554423c77f9b2dfda1b50
Johannes Brodwall <johannes@brodwall.com>
2024-10-02 20:16:40 +0300

    Prepare for Heroku: Build client and run server with reference to client code

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import { settlementsApi } from "./settlementsApi";
 import { MongoClient } from "mongodb";
+import * as path from "node:path";
 
 const app = express();
 app.use(express.json());
-app.listen(3000);
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+app.listen(process.env.PORT || 3000);
 
 const mongoClient = new MongoClient(
   process.env.MONGO_URI || "mongodb://localhost:27017/settlements",
