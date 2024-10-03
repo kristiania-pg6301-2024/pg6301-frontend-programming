99f497edee7ac41e213480e4853347fa71a5963a
Johannes Brodwall <johannes@brodwall.com>
2024-10-03 14:33:15 +0300

    LoginRedirect step 4: add client_id query parameter

--- client/src/components/auth/loginRedirect.tsx
+++ client/src/components/auth/loginRedirect.tsx
@@ -6,7 +6,8 @@ export function LoginRedirect() {
   useEffect(() => {
     const authorizationEndpoint =
       "https://login.microsoftonline.com/fb35ccd7-e4c1-47fc-9f1d-4aed73c1df20/oauth2/v2.0/authorize";
-    const query = { q: "openid connect" };
+    const client_id = "25fc066e-1a1c-4bbe-a56c-30e47dc4ac28";
+    const query = { client_id };
     setAuthorizationUri(
       `${authorizationEndpoint}?${new URLSearchParams(query)}`,
     );
