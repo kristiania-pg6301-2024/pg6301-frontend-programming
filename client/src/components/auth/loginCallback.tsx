1a5f760f3dbbaed9a5ed39bcd750b4cb57acaa68
Johannes Brodwall <johannes@brodwall.com>
2024-10-03 14:40:46 +0300

    LoginCallback: add error handling with hash

--- client/src/components/auth/loginCallback.tsx
+++ client/src/components/auth/loginCallback.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export function LoginCallback() {
   const params = Object.fromEntries(
-    new URLSearchParams(window.location.search).entries(),
+    new URLSearchParams(
+      window.location.hash?.substring(1) || window.location.search,
+    ).entries(),
   );
 
   if ("error" in params) {
