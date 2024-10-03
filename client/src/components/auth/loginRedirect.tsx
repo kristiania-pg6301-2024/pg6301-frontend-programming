a087b6154bd253bf0f15f0d3fa785e200277ffb2
Johannes Brodwall <johannes@brodwall.com>
2024-10-03 14:31:16 +0300

    LoginRedirect step 3: redirect to EntryID endpoint

--- client/src/components/auth/loginRedirect.tsx
+++ client/src/components/auth/loginRedirect.tsx
@@ -4,7 +4,8 @@ import { ProgressIndicator } from "../progressIndicator";
 export function LoginRedirect() {
   const [authorizationUri, setAuthorizationUri] = useState<string>();
   useEffect(() => {
-    const authorizationEndpoint = "https://www.google.com/search";
+    const authorizationEndpoint =
+      "https://login.microsoftonline.com/fb35ccd7-e4c1-47fc-9f1d-4aed73c1df20/oauth2/v2.0/authorize";
     const query = { q: "openid connect" };
     setAuthorizationUri(
       `${authorizationEndpoint}?${new URLSearchParams(query)}`,
