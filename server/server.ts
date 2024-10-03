4bf260130a6882ddf8f798bfc026d9b5a35de51c
Johannes Brodwall <johannes@brodwall.com>
2024-10-03 17:48:27 +0300

    Save access_token cookie in POST /api/login

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 app.get("/api/login", (_, res) => {
   res.status(401).json({ error: "Unauthorized" });
+});
+app.post("/api/login", (req, res) => {
+  const { access_token } = req.body;
+  res.cookie("access_token", access_token).redirect(`/`);
 });
 app.listen(3000);
