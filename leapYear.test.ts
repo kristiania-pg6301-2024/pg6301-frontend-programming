414813783c50986c1bda284b893148bfbe1eb395
Johannes Brodwall <johannes@brodwall.com>
2024-10-09 12:51:40 +0200

    add prettier

--- leapYear.test.ts
+++ leapYear.test.ts
@@ -3,17 +3,16 @@ import { isLeapYear } from "./isLeapYear";
 
 test("should detect normal leap years", () => {
   expect(isLeapYear(2024)).toBe(true);
-})
+});
 
 test("should detect non-leap years", () => {
   expect(isLeapYear(2025)).toBe(false);
-})
+});
 
 test("that most years divisible by 100 are not leap years", () => {
   expect(isLeapYear(2100)).toBe(false);
-})
+});
 
 test("that years divisible by 400 are leap years", () => {
   expect(isLeapYear(2000)).toBe(true);
-})
-
+});
