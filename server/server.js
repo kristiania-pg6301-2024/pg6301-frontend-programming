7130e47285ab219ce4fe45094bd1c6a6ec387027
Johannes Brodwall <johannes@brodwall.com>
2024-10-23 10:23:20 +0200

    read cookie in GET /api/userinfo

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.get("/api/userinfo", (req, res) => {
+  console.log(req.cookies["accessToken"]);
   res.sendStatus(401); // Unauthorized
 });
+
 app.post("/api/login", (req, res) => {
   const { accessToken } = req.body;
   res.cookie("accessToken", accessToken, { httpOnly: true });
