92481c3d1cf2e06ae965c076cb90f4fc74791c3e
Johannes Brodwall <johannes@brodwall.com>
2024-10-23 09:45:29 +0200

    load user info

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>);
+
+function useUserInfo() {
+  const [userinfo, setUserinfo] = useState();
+  const [error, setError] = useState();
+
+  async function loadUserInfo() {
+    try {
+      const res = await fetch("/api/userinfo");
+      if (res.ok) {
+        setUserinfo(await res.json());
+      } else {
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      setError(e);
+    }
+  }
+
+  useEffect(() => {
+    loadUserInfo();
+  }, []);
+
+  return { userinfo, error };
+}
+
+function Application() {
+  const { userinfo, error } = useUserInfo();
+
+  if (error) {
+    return (
+      <>
+        <h1>En feil har inntruffet</h1>
+        <div>{error.toString()}</div>
+      </>
+    );
+  }
+
+  return <h1>Hello User</h1>;
+}
+
+root.render(<Application />);
