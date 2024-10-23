529d99792619477fad2d2c7fd616666a10594cb0
Johannes Brodwall <johannes@brodwall.com>
2024-10-23 10:44:09 +0200

    simplify fetching userinfo

--- server/server.js
+++ server/server.js
@@ -5,26 +5,28 @@ const app = express();
 app.use(express.urlencoded());
 app.use(cookieParser());
 
-app.get("/api/userinfo", async (req, res) => {
-  const { accessToken } = req.cookies;
-  console.log(accessToken);
-
-  const openidConfigurationRes = await fetch(
-    "https://accounts.google.com/.well-known/openid-configuration",
-  );
-  const openidConfiguration = await openidConfigurationRes.json();
-  const { userinfo_endpoint } = openidConfiguration;
-
-  const userinfoRes = await fetch(userinfo_endpoint, {
-    headers: { Authorization: `Bearer ${accessToken}` },
-  });
-  if (userinfoRes.ok) {
-    const userinfo = await userinfoRes.json();
-    console.log(userinfo);
-    return res.json(userinfo);
+async function fetchJson(url, options) {
+  const res = await fetch(url, options);
+  if (!res.ok) {
+    throw new Error(`${res.status} ${res.statusText}`);
   }
+  return await res.json();
+}
 
-  res.sendStatus(401); // Unauthorized
+app.get("/api/userinfo", async (req, res) => {
+  try {
+    const { accessToken } = req.cookies;
+    const { userinfo_endpoint } = await fetchJson(
+      "https://accounts.google.com/.well-known/openid-configuration",
+    );
+    const userinfo = await fetchJson(userinfo_endpoint, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    res.json(userinfo);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 app.post("/api/login", (req, res) => {
