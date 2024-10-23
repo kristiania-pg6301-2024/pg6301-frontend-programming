790514d9ab6a95dbb472a0d51ca40ebed243c0f7
Johannes Brodwall <johannes@brodwall.com>
2024-10-23 10:21:49 +0200

    set cookie on server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 
 const app = express();
+app.use(express.urlencoded());
+
 app.get("/api/userinfo", (req, res) => {
   res.sendStatus(401); // Unauthorized
+});
+app.post("/api/login", (req, res) => {
+  const { accessToken } = req.body;
+  res.cookie("accessToken", accessToken, { httpOnly: true });
+  res.sendStatus(200);
 });
 app.listen(process.env.PORT || 3000);
