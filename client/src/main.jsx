d10600d95e18f3e49fa6bf7d01c40dde1b1853df
Johannes Brodwall <johannes@brodwall.com>
2024-10-23 12:37:04 +0200

    fetch user from client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello Who Are You</h1>);
+
+function Application() {
+  const [user, setUser] = useState();
+
+  async function loadUser() {
+    const res = await fetch("/api/userinfo");
+    setUser(await res.json());
+  }
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  if (user) {
+    return <h1>You are {user.name}</h1>;
+  }
+
+  return <h1>Hello Who Are You</h1>;
+}
+
+root.render(<Application />);
