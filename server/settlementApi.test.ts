88ed6442475eb58586e3d0438375ec3ae956ee1a
Johannes Brodwall <johannes@brodwall.com>
2024-10-30 13:51:31 +0100

    completed implementation of router

--- server/settlementApi.test.ts
+++ server/settlementApi.test.ts
@@ -8,19 +8,25 @@ const allSettlements: object[] = [];
 settlementRouter.get("/", (req, res) => {
   res.json(allSettlements);
 });
+settlementRouter.post("/", (req, res) => {
+  const { department, balance } = req.body;
+  allSettlements.push({ department, balance });
+  res.sendStatus(201);
+});
 
 const app = express();
+app.use(express.json());
 app.use(settlementRouter);
 
 test("that submitted settlements are listed", async () => {
   const newSettlement = {
-    department: "test dep",
+    department: "test dep " + new Date(),
     balance: { "1000kr": 1000 },
   };
 
-  allSettlements.push({ ...newSettlement });
+  await request(app).post("/").send(newSettlement).expect(201);
 
-  request(app)
+  await request(app)
     .get("/")
     .expect(200)
     .then((response) => {
