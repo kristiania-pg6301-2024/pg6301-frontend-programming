180a8364d72a41df346222ec7553a2e91ae6bb2a
Johannes Brodwall <johannes@brodwall.com>
2024-10-30 13:44:46 +0100

    initial test with supertest

--- server/settlementApi.test.ts
+++ server/settlementApi.test.ts
@@ -1,14 +1,29 @@
 import { expect, test } from "vitest";
+import express from "express";
+import request from "supertest";
 
-test("that submitted settlements are listed", () => {
+const settlementRouter = express.Router();
+const allSettlements: object[] = [];
+
+settlementRouter.get("/", (req, res) => {
+  res.json(allSettlements);
+});
+
+const app = express();
+app.use(settlementRouter);
+
+test("that submitted settlements are listed", async () => {
   const newSettlement = {
     department: "test dep",
     balance: { "1000kr": 1000 },
   };
-  const allSettlements: object[] = [];
 
-  // this is what I want the router to do:
   allSettlements.push({ ...newSettlement });
 
-  expect(allSettlements).toContainEqual(newSettlement);
+  request(app)
+    .get("/")
+    .expect(200)
+    .then((response) => {
+      expect(response.body).toContainEqual(newSettlement);
+    });
 });
