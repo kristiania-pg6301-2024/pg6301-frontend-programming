c0a0b0a9abeff50bc4c3ab573f82e2f1867d7bf3
Johannes Brodwall <johannes@brodwall.com>
2024-11-05 23:36:22 +0100

    GET /api/login/linkedin/callback fetch token

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 
+import dotenv from "dotenv";
+
+dotenv.config({ debug: true });
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -13,13 +17,13 @@ async function fetchJson(url) {
 app.get("/api/login/linkedin/start", async (req, res) => {
   const discoveryEndpoint =
     "https://www.linkedin.com/oauth/.well-known/openid-configuration";
+  const { authorization_endpoint } = await fetchJson(discoveryEndpoint);
   const client_id = "7792wb3of776if";
   const redirect_uri = `${req.protocol}://${req.headers.host}/api/login/linkedin/callback`;
 
-  const { authorization_endpoint } = await fetchJson(discoveryEndpoint);
   const authorization_url = `${authorization_endpoint}?${new URLSearchParams({
     response_type: "code",
-    scope: "profile email",
+    scope: "profile email openid",
     client_id,
     redirect_uri,
   })}`;
@@ -27,12 +31,41 @@ app.get("/api/login/linkedin/start", async (req, res) => {
 });
 
 app.get("/api/login/linkedin/callback", async (req, res) => {
-  const { error, error_description } = req.query;
+  const { code, error, error_description } = req.query;
   if (error) {
     res.json({
       error,
       error_description,
     });
+  } else if (code) {
+    const discoveryEndpoint =
+      "https://www.linkedin.com/oauth/.well-known/openid-configuration";
+    const client_id = "7792wb3of776if";
+    const client_secret = process.env.LINKEDIN_CLIENT_SECRET;
+    const redirect_uri = `${req.protocol}://${req.headers.host}/api/login/linkedin/callback`;
+    const { token_endpoint } = await fetchJson(discoveryEndpoint);
+    const request = {
+      code,
+      grant_type: "authorization_code",
+      client_id,
+      client_secret,
+      redirect_uri,
+    };
+    const response = await fetch(token_endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(request).toString(),
+    });
+    if (response.status === 200) {
+      const responseJson = await response.json();
+      const { access_token } = responseJson;
+      res.cookie("linkedin_access_token", access_token);
+      res.redirect("/");
+    } else {
+      res.status(response.status).json(await response.json());
+    }
   } else {
     res.status(500).send("Don't know how to deal with this request");
   }
