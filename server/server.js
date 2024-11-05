c9d6c089f7347b7981b59c8f8c124d9278052661
Johannes Brodwall <johannes@brodwall.com>
2024-11-05 22:55:42 +0100

    GET /api/login/linkedin/start and GET /api/login/linkedin/callback

--- server/server.js
+++ server/server.js
@@ -5,14 +5,46 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+async function fetchJson(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+
+app.get("/api/login/linkedin/start", async (req, res) => {
+  const discoveryEndpoint =
+    "https://www.linkedin.com/oauth/.well-known/openid-configuration";
+  const client_id = "7792wb3of776if";
+  const redirect_uri = `${req.protocol}://${req.headers.host}/api/login/linkedin/callback`;
+
+  const { authorization_endpoint } = await fetchJson(discoveryEndpoint);
+  const authorization_url = `${authorization_endpoint}?${new URLSearchParams({
+    response_type: "code",
+    scope: "profile email",
+    client_id,
+    redirect_uri,
+  })}`;
+  res.redirect(authorization_url);
+});
+
+app.get("/api/login/linkedin/callback", async (req, res) => {
+  const { error, error_description } = req.query;
+  if (error) {
+    res.json({
+      error,
+      error_description,
+    });
+  } else {
+    res.status(500).send("Don't know how to deal with this request");
+  }
+});
+
 app.get("/api/userinfo", async (req, res) => {
   const { access_token } = req.cookies;
 
   if (access_token) {
     const discoveryEndpoint =
       "https://accounts.google.com/.well-known/openid-configuration";
-    const configuration = await fetch(discoveryEndpoint);
-    const { userinfo_endpoint } = await configuration.json();
+    const { userinfo_endpoint } = await fetchJson(discoveryEndpoint);
     const userinfoRes = await fetch(userinfo_endpoint, {
       headers: {
         Authorization: `Bearer ${access_token}`,
